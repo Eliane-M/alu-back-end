3f12ee09289e881ef9c8b780f7a4bb6f57993352
Eliane-M <munezeroeliane761@gmail.com>
2023-11-05 10:07:46 -0800

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,12 +5,12 @@ get number of done tasks by users
 """
 
 
-def main(employee_todo_progress):
+def main():
     import json
     import sys
     import urllib.request
 
-    employee_id = sys.arg[1]
+    employee_id = sys.argv[1]
     url1 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
     url2 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/"
 
@@ -26,13 +26,16 @@ def main(employee_todo_progress):
     for task in response1:
         if task['completed'] is not True:
             continue
-        completed_task.append(task)
+        completed_tasks.append(task)
 
     number_of_completed = len(completed_tasks)
     number_of_all_tasks = len(response1)
     employee_name = response2["name"]
 
     print(f"Employee {employee_name} is done with tasks \
-            ({number_of_completed}/{number_of_all_tasks}")
+            ({number_of_completed}/{number_of_all_tasks}):")
     for tasks in completed_tasks:
-        print(f"\t {comp_tasks['title']}")
+        print(f"\t {tasks['title']}")
+
+if __name__ == "__main__":
+    main()
